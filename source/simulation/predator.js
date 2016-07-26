bd59a5c6807477174773d7633b135d0bca423fe6
Adam Siler <lasiler@gmail.com>
2016-07-26 11:56:15 -0600

    Move predator default opts onto prototype.

--- source/simulation/predator.js
+++ source/simulation/predator.js
@@ -1,22 +1,21 @@
 import Physics from '../engine/physics'
 
-const defaultOpts = {
-	radius:  128,
-	mass:    100,
-	drag:    10,
-	elasticity: 7,
-	speed:  170,
-}
-
 export default function Predator( opts = {} ) {
 	const self = Object.create( Predator.prototype )
+	
 	Physics( self )
-	Object.assign( self, defaultOpts, opts )
+	Object.assign( self, opts )
 	
 	return self
 }
 
 Predator.prototype = {
+	drag:        10,
+	elasticity:   7,
+	mass:       100,
+	radius:     128,
+	speed:      120,
+	
 	update( dt ) {
 		this.updatePhysics( dt )
 	},
