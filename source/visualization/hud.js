537076d706e9fab7a2f9dc0184592f623b85b36c
Adam Siler <lasiler@gmail.com>
2016-07-28 15:53:34 -0600

    Animate focus ring and crosshairs.

--- source/visualization/hud.js
+++ source/visualization/hud.js
@@ -1,3 +1,18 @@
+const interpolate = ( startValue, endValue, bias ) => {
+	return startValue + ( endValue - startValue ) * bias
+}
+
+const interpolateRgba = ( rgba1, rgba2, bias ) => {
+	const rgba3 = []
+	
+	for ( let i = 0; i < rgba1.length; i++ ) {
+		rgba3[i] = Math.round( interpolate( rgba1[i], rgba2[i], bias ) )
+	}
+	
+	const [ r, g, b, a ] = rgba3
+	return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'
+}
+
 export default function Hud() {
 	const self = {}
 	
@@ -9,6 +24,9 @@ export default function Hud() {
 		Object.assign( focusRing.position, position )
 		focusRing.radius = radius
 		
+		angle = 0
+		bias = 0
+		
 		self.focusRing = focusRing
 	}
 	
@@ -16,8 +34,12 @@ export default function Hud() {
 		self.focusRing = null
 	}
 	
+	let angle = 0
+	let bias = 0
+	
 	self.update = ( dt ) => {
-		// ...
+		angle = ( angle + Math.PI * 1 * dt ) % Math.PI
+		bias = 1 - ( 1 + Math.cos( angle ) ) * 0.5
 	}
 	
 	self.draw = ( camera ) => {
@@ -27,16 +49,20 @@ export default function Hud() {
 		
 		const globalAlpha = ctx.globalAlpha
 		ctx.globalAlpha = 0.8
+		const globalCompositeOperation = ctx.globalCompositeOperation
+		ctx.globalCompositeOperation = 'lighten'
 		
 		if ( self.focusRing ) {
 			const p = self.focusRing.position
-			const r2 = r1 + 16 / z
 			const r1 = self.focusRing.radius + 18
+			const r2 = r1 + 18 / z
+			
+			const color = interpolateRgba( [ 255, 128, 0, 1 ], [ 255, 69, 0, 1 ], bias )
 			
 			ctx.beginPath()
 				ctx.arc( p.x, p.y, r1, 0, tau )
 				ctx.arc( p.x, p.y, r2, 0, tau, true )
-				ctx.fillStyle = 'orange'
+				ctx.fillStyle = color
 				ctx.fill()
 			
 			ctx.beginPath()
@@ -44,12 +70,13 @@ export default function Hud() {
 					ctx.moveTo( p.x + Math.cos( angle ) * ( r2 + 5/z ), p.y + Math.sin( angle ) * ( r2 + 5/z ) )
 					ctx.lineTo( p.x + Math.cos( angle ) * r2 * 20,  p.y + Math.sin( angle ) * r2 * 20 )
 				}
-				ctx.strokeStyle = 'orange'
+				ctx.strokeStyle = color
 				ctx.lineWidth = 2 / z
 				ctx.stroke()
 		}
 		
 		ctx.globalAlpha = globalAlpha
+		ctx.globalCompositeOperation = globalCompositeOperation
 	}
 	
 	return self
