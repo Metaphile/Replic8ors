411229bb9a18ca06be0b90fd251139ccf4db18d1
Adam Siler <lasiler@gmail.com>
2016-08-11 13:01:51 -0600

    Removed timescale component from play controls. Wasn't that useful.

--- source/play-controls.js
+++ source/play-controls.js
@@ -18,34 +18,12 @@ export default function PlayControls( gameLoop ) {
 	
 	// TODO fast-forward
 	
-	// TODO get rid of this thing
-	// use keys/buttons to slow down or speed up time
-	const $timescale =
-		$( '<input name="timescale" type="range" min="0.1" step="0.1" max="12.7">' )
-	
-	{
-		const curve     = x => Math.pow( x / 4, 2 )
-		const curve_inv = y => Math.pow( y, 1/2 ) * 4
-		
-		$timescale.on( 'input', () => {
-			gameLoop.timescale = curve( $timescale.val() )
-		} )
-		
-		// custom event
-		$timescale.on( 'timescale-changed', () => {
-			$timescale.val( curve_inv( gameLoop.timescale ) )
-		} )
-		
-		$timescale.trigger( 'timescale-changed' )
-	}
-	
 	const $sloMo =
 		$( '<button name="slo-mo">0.25x</button>' )
 	
 	$sloMo.click( () => {
 		gameLoop.timescale = 0.25
 		gameLoop.paused = false
-		$timescale.trigger( 'timescale-changed' )
 	} )
 	
 	const $normalSpeed =
@@ -54,7 +32,6 @@ export default function PlayControls( gameLoop ) {
 	$normalSpeed.click( () => {
 		gameLoop.timescale = 1
 		gameLoop.paused = false
-		$timescale.trigger( 'timescale-changed' )
 	} )
 	
 	const $highSpeed =
@@ -63,7 +40,6 @@ export default function PlayControls( gameLoop ) {
 	$highSpeed.click( () => {
 		gameLoop.timescale = 10
 		gameLoop.paused = false
-		$timescale.trigger( 'timescale-changed' )
 	} )
 	
 	$playPause.click( () => {
@@ -83,21 +59,9 @@ export default function PlayControls( gameLoop ) {
 				$playPause.click()
 				break
 			
-			case 38: // up arrow
-				event.preventDefault()
-				$timescale.val( +$timescale.val() + +$timescale.attr( 'step' ) )
-				$timescale.trigger( 'input' )
-				break
-			
 			case 39: // right arrow
 				event.preventDefault() // may be too much
 				$step.click()
-				break
-			
-			case 40: // down arrow
-				event.preventDefault()
-				$timescale.val( $timescale.val() - $timescale.attr( 'step' ) )
-				$timescale.trigger( 'input' )
 				break
 		}
 	} )
@@ -106,7 +70,6 @@ export default function PlayControls( gameLoop ) {
 	$form.append( $step )
 	$form.append( $sloMo )
 	$form.append( $normalSpeed )
-	$form.append( $timescale )
 	$form.append( $highSpeed )
 	
 	return $form[ 0 ]
