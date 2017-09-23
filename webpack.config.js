3cfb7817890512b3f086ac163ce9ae4e4b14148a
Adam Siler <lasiler@gmail.com>
2017-09-23 12:51:28 -0600

    webpack handles index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: './source/main.js',
@@ -6,6 +7,12 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Replic8ors - Behavioral evolution in your browser',
+			template: 'source/index.ejs'
+		})
+	],
 	module: {
 		rules: [{
 				test: /\.scss$/,
