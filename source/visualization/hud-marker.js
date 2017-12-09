c21d91eef697fff0905825cb60cbc8e6958de238
Adam Siler <lasiler@gmail.com>
2017-12-09 15:00:53 -0700

    made HUD redder

--- source/visualization/hud-marker.js
+++ source/visualization/hud-marker.js
@@ -36,7 +36,7 @@ Marker.prototype = {
 			ctx.lineTo( base3.x, base3.y )
 			ctx.closePath()
 			
-			ctx.fillStyle = 'rgba( 255, 60, 0, 0.51 )'
+			ctx.fillStyle = 'rgba( 255, 30, 0, 0.51 )'
 			ctx.fill()
 	}
 }
