cdcdd90161c0734e8dfebb6a7f4e0b7b87f90447
Adam Siler <lasiler@gmail.com>
2017-12-17 16:11:33 -0700

    logo colors are now based on excitatory/inhibitory colors

--- source/config.js
+++ source/config.js
@@ -1,6 +1,6 @@
 export default {
-	excitatoryColor: 'rgba(  90, 195, 255, 1.0 )',
-	inhibitoryColor: 'rgba( 110,   0,   0, 1.0 )',
+	excitatoryColor: 'rgba(  90, 195, 255, 1.0 )', // logo assumes rgba(  90, 195, 255, 1.0 )
+	inhibitoryColor: 'rgba( 110,   0,   0, 1.0 )', // logo assumes rgba( 110,   0,   0, 1.0 )
 	// default is 'source-over'
 	excitatoryCompositeOperation: 'screen',
 	inhibitoryCompositeOperation: 'darken',
