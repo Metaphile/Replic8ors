79b1a92aa49056ca56f9e4b0dac362dcd1e93806
Adam Siler <lasiler@gmail.com>
2019-08-25 23:33:31 -0400

    also constrain food particles to world radius

--- source/simulation/world.js
+++ source/simulation/world.js
@@ -260,9 +260,9 @@ World.prototype = {
 			predator.update( dt )
 		}
 		
-		// constrain replicator to radius
-		for ( const replicator of [ ...predators, ...replicators ] ) {
-			const p = replicator.position
+		// constrain entities to radius
+		for ( const entity of [ ...predators, ...replicators, ...foods ] ) {
+			const p = entity.position
 			const dist = Vector2.getLength( p )
 			if ( dist > this.radius ) {
 				Vector2.setLength( p, this.radius )
